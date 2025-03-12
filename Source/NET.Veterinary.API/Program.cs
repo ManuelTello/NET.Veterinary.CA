92fa64cf55eafc7a59b3b5cde148b40dc870f443
manuel <manuelTello.business@gmail.com>
2025-03-12 18:30:34 -0300

    Swagger added to API project

--- Source/NET.Veterinary.API/Program.cs
+++ Source/NET.Veterinary.API/Program.cs
@@ -6,12 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddInfrastructure(builder.Configuration).AddApplication();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
